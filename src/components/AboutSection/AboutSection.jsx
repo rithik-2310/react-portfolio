f38ed10ee9ca303b52b93c2d3fd854642218ca41
rithik-2310 <rithiksaru@gmail.com>
2026-08-20 13:24:20 +0530

    photo_change

--- src/components/AboutSection/AboutSection.jsx
+++ src/components/AboutSection/AboutSection.jsx
@@ -5,7 +5,7 @@ function AboutSection() {
     <section id='about' className='about-section'>
       <div className='about-wrapper container'>
         <div className='about-image'>
-          <img src='public/profile.jpeg' alt='Rithik' />
+          <img src='/profile.jpeg' alt='Rithik' />
         </div>
         <div className='about-copy'>
           <h1>About Me</h1>
